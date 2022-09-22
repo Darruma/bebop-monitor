05eb731bb9653b4316677a2918f056607ab3c497
Swole Badger <swole@badger.finance>
2022-09-22 21:35:50 +0100

    feat: add arrows

--- components/OrderInfo.tsx
+++ components/OrderInfo.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-
+import Xarrow from "react-xarrows";
 
 const OrderInfoWrapper = styled.div`
     display: flex;
@@ -27,24 +27,49 @@ function OrderInfo(props) {
     const quoteTokens = props.order.quoteTokens.slice().sort((a, b) => {
         return Number(a.id[0]) > Number(b.id[0])
     })
-    
+    let arrows;
+    if (baseTokens.length >= quoteTokens.length) {
+        arrows = baseTokens.map((bt, index) => {
+            return <Xarrow
+                startAnchor={"right"}
+                endAnchor={"left"}
+                start={`order-input-${props.order.id}-${index}`} end={`order-output-${props.order.id}`} />
+        })
+    }
+    if (quoteTokens.length > baseTokens.length) {
+        arrows = quoteTokens.map((bt, index) => {
+            return <Xarrow
+                startAnchor={"right"}
+                endAnchor={"left"}
+                start={`order-input-${props.order.id}`} end={`order-output-${props.order.id}-${index}`} />
+        })
+    }
+
     return <OrderInfoWrapper>
         <OrderInput>
             {baseTokens.map((bt, index) => {
+                let outputId = (quoteTokens.length > baseTokens.length) ? `order-input-${props.order.id}` : `order-input-${props.order.id}-${index}`
                 const amount = props.order.baseAmounts[index] / Math.pow(10, bt.decimals)
                 return (<OrderData>
-                    {amount.toLocaleString()} {bt.symbol}
+                    <span id={outputId}>
+                        {amount.toLocaleString()} {bt.symbol}
+                    </span>
                 </OrderData>)
             })}
         </OrderInput>
+
         <OrderOutput>
             {quoteTokens.map((qt, index) => {
+                let outputId = (quoteTokens.length <= baseTokens.length) ? `order-output-${props.order.id}` : `order-output-${props.order.id}-${index}`
                 const amount = props.order.quoteAmounts[index] / Math.pow(10, qt.decimals)
                 return (<OrderData>
-                    {amount.toLocaleString()} {qt.symbol}
+                    <span id={outputId}>
+                        {amount.toLocaleString()} {qt.symbol}
+                    </span>
                 </OrderData>)
             })}
         </OrderOutput>
+        {arrows}
     </OrderInfoWrapper>
 }
 
